410cd18ac3217e3e076b91f63fee3c08c1375e88
mahcodes <mhmdali102@bk.ru>
2024-04-18 09:04:46 +0300

    cleanup

--- guwee/src/main.js
+++
@@ -1,4 +0,0 @@
-import "./styles/global.css";
-import { main } from "./guwee.gleam";
-
-main();
--- guwee/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{gleam,js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
